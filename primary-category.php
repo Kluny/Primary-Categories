abcc0e6811cea740bed40b224bf324fea32391c8
Shannon Graham <shannon@Shannons-MacBook-Pro.local>
2017-10-05 14:35:17 -0700

    Updates to ensure compliance with spec
    - added method to remove Primary Category if none is selected
    - added code to ensure that it works with custom post types
    - added allowance for specifying the desired post type in shortcode

--- primary-category.php
+++ primary-category.php
@@ -3,86 +3,93 @@
  * @package Primary_Category
  * @author Shannon Graham <shannon@rocketships.ca>
  * @license  GPLv2 or later
- * @link www.rocketships.ca
+ * @link http://rocketships.ca
  */
 /*
-Plugin Name: Primary Category
-Plugin URI: https://rocketships.ca
-Description:
-Many publishers use categories as a means to logically organize their content. However, many pieces of content have more than one category. Sometimes it’s useful to designate a primary category for posts (and custom post types).
-Version: 0.1
-Author: Shannon Graham
-Author URI: https://rocketships.ca
-License: GPLv2 or later
-Text Domain: pc
+    Plugin Name: Primary Category
+    Plugin URI: http://rocketships.ca
+    Description: Many publishers use categories as a means to logically organize their content.
+        However, many pieces of content have more than one category.
+        Sometimes it’s useful to designate a primary category for posts (and custom post types).
+    Version: 0.1
+    Author: Shannon Graham
+    Author URI: http://rocketships.ca
+    License: GPLv2 or later
+    Text Domain: pc
 */
 
 /*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
 
 // Make sure we don't expose any info if called directly
-if (!function_exists('add_action') ) {
+if ( !function_exists('add_action') ) {
     echo "Hi there!  I'm just a plugin, not much I can do when called directly.";
     exit;
 }
 
+// Standard info to make the codesniffer happy
+define( 'PRIMARY_CATEGORY_VERSION', '0.1' );
+define( 'PRIMARY_CATEGORY__MINIMUM_WP_VERSION', '4.0' );
+define( 'PRIMARY_CATEGORY__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
 /**
  * Add custom taxonomy
  */
-function pc_create_primary_category_taxonomy() 
+function pc_register_taxonomy()
 {
     register_taxonomy(
-        'primary_category', array( 'post' ), array(
+        'primary_category',  get_post_types([], 'names'), array( //this taxonomy applies to all post types; possible improvement, make it a setting to include specified post types.
         'hierarchical'      => false,
         'show_ui'           => true,
         'show_admin_column' => true,
         'query_var'         => true,
         'labels'            => array( 'name' => "Primary Category" ),
         'capabilities'      => array('manage_terms'),
-        'meta_box_cb'       => 'pc_primary_category_markup'
+        'meta_box_cb'       => 'pc_metabox_markup'
         ) 
     );
 }
-add_action('init', 'pc_create_primary_category_taxonomy', 0);
+add_action('init', 'pc_register_taxonomy', 0);
 
 
 /**
- * Adds a select menu to metabox with all existing categories.
+ * Adds a select menu to metabox with all existing categories. Metabox does not update in realtime a new category is added; possible future improvement.
  *
  * @param $object - The current post
  */
-function pc_primary_category_markup( $object )
+function pc_metabox_markup( $object )
 {
 
     get_post_meta($object->ID);
 
-    $selected = -1;
-    if($selected = get_the_terms($object->ID, "primary_category", true) ) {
+    if($selected = get_the_terms($object->ID, 'primary_category') ) {
         if(! is_wp_error($selected) ) {
             if($category = get_category_by_slug($selected[0]->slug) ) {
                    $selected = $category->term_id;
             }
+        } else {
+            $selected = -1;
         }
     }
 
-    wp_nonce_field(basename(__FILE__), "primary-category-nonce");
+    wp_nonce_field(basename(__FILE__), 'primary-category-nonce');
 
     //having "Uncategorized" as a primary category doesn't seem useful to me.
-    if($uncategorized = get_category_by_slug("uncategorized") ) {
+    if($uncategorized = get_category_by_slug('uncategorized') ) {
         $uncategorized = $uncategorized->term_id;
     }
 
@@ -91,10 +98,10 @@ function pc_primary_category_markup( $object )
     <div><?php
     wp_dropdown_categories(
         [
-        "name" => "primary-category",
-        "selected" => $selected,
-        "show_option_none" => "none",
-        "exclude" => $uncategorized
+        'name' => "primary-category",
+        'selected' => $selected,
+        'show_option_none' => "none",
+        'exclude' => $uncategorized
         ]
          ); ?>
     </div>
@@ -103,37 +110,39 @@ function pc_primary_category_markup( $object )
 
 
 /**
- * Saves the primary category as postmeta
+ * Saves the primary category as a custom taxonomy
  *
- * @param  $post_id
+ * @param $post_id
+ * @param $post
  * @return mixed - returns $post_id on failure
  */
 
-function pc_save_primary_category( $post_id )
+function pc_save_primary_category( $post_id, $post )
 {
     //ignore autosave
-    if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE ) {
+    if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
         return $post_id;
     }
 
     //check nonce
-    if (! isset($_POST["primary-category-nonce"]) || ! wp_verify_nonce($_POST["primary-category-nonce"], basename(__FILE__)) ) {
+    if (! isset($_POST['primary-category-nonce']) || ! wp_verify_nonce($_POST['primary-category-nonce'], basename(__FILE__)) ) {
         return $post_id;
     }
 
     //check user's capabilities
-    if(!current_user_can("edit_post", $post_id) ) {
-        return $post_id;
-    }
-
-    //make sure it's a post and not something else
-    if("post" !== $post->post_type ) {
+    if(!current_user_can('edit_post', $post_id) ) {
         return $post_id;
     }
 
     //make sure there's input
     if(! isset($_POST['primary-category']) ) {
         return $post_id;
+    }
+
+    // if the "none" option was selected
+    if($_POST['primary-category'] === "-1") {
+        pc_remove_primary_category($post_id);
+		return $post_id;
     }
 
     //make sure the category exists
@@ -146,36 +155,48 @@ function pc_save_primary_category( $post_id )
         return $post_id;
     }
 
-    //save primary category as custom taxonomy here
-    wp_set_post_terms($post_id, $selected->slug, 'primary_category', $append = false);
-
-    // add post to category if it's not there already
-    if(! has_category($selected->term_id, $post_id)  ) {
-        wp_set_post_categories($post_id, $selected->term_id, true);
+    //save primary category as custom taxonomy
+    if(false !== wp_set_post_terms($post_id, $selected->slug, 'primary_category', $append = false) ) {
+        // add post to category if it's not there already
+        if(! has_category($selected->term_id, $post_id) ) {
+            wp_set_post_categories($post_id, $selected->term_id, true);
+        }
     }
-
 }
-add_action('save_post', 'pc_save_primary_category');
-
+add_action('save_post', 'pc_save_primary_category', 10, 2);
 
 /**
- * Returns array of posts with given primary category
+ * Removes primary category from a post. This plugin assumes a post can have one and only one primary category and therefore removes only the first one found. If a bug occurs where multiple primary terms exist, this method could be modified to iterate terms instead.
+ *
+ * @param $post_id
+ */
+function pc_remove_primary_category( $post_id ) {
+	if($term = get_the_terms($post_id, 'primary_category') ) {
+		if(! is_wp_error($term) ) {
+            wp_remove_object_terms($post_id, $term[0]->term_id, 'primary_category');
+		}
+	}
+}
+
+/**
+ * Returns array of posts with given primary category and post type.
  *
  * @param $category - Category slug
+ * @param $post_type {string|array}
  *
- * @return false|WP_Query
+ * @return false|array
  */
-function pc_primary_category_posts( $category ) 
+function pc_primary_category_posts( $category, $post_type = 'post' )
 {
-
     $posts_array = get_posts(
         array(
         'posts_per_page' => -1,
-        'post_type' => 'post',
-        'tax_query' => array(
+        'post_type' => $post_type,
+		'suppress_filters' => false,
+        'tax_query' => array( // this can potentially cause a slow query. If so, caching can be added to this function.
                 array(
-                    'taxonomy' => 'primary_category',
-                    'field' => 'slug',
+                    'taxonomy' => "primary_category",
+                    'field' => "slug",
                     'terms' => $category,
                 )
             )
@@ -186,7 +207,8 @@ function pc_primary_category_posts( $category )
 }
 
 /**
- * Return HTML for displaying terms of a given category. Set 'category' value in attributes array. Intended for use by primary-category shortcode.
+ * Return HTML for displaying terms of a given category. Set 'category' value in attributes array.
+ * Intended for use by primary-category shortcode.
  *
  * @param $attributes
  *
@@ -195,18 +217,22 @@ function pc_primary_category_posts( $category )
 function pc_display_posts( $attributes ) 
 {
 
-    if(empty($attributes[ 'category' ]) ) {
+    if( empty($attributes[ 'category' ]) ) {
         return false;
     }
+	$category = sanitize_text_field( $attributes[ 'category' ]);
 
-    $posts = pc_primary_category_posts($attributes[ 'category' ]);
+    $post_type = ( empty($attributes[ 'post_type' ]) ) ? 'post' : sanitize_text_field($attributes[ 'post_type' ]);
+    $post_type = explode(',', $post_type);
+
+    $posts = pc_primary_category_posts($category, $post_type);
 
     $display = '<ul>';
-    foreach($posts as $post) {
+    foreach( $posts as $post ) {
         $display .= '<li><a href="' . get_permalink($post) . '">' . $post->post_title . '</a></li>';
     }
 
-    return $display . "</ul>";
+    return $display . '</ul>';
 }
 add_shortcode('primary-category', 'pc_display_posts');
 
@@ -220,7 +246,7 @@ add_shortcode('primary-category', 'pc_display_posts');
 function pc_get_primary_category( $post_id )
 {
 
-    if($primary_category = get_the_terms($post_id, "primary_category", true) ) {
+    if($primary_category = get_the_terms($post_id, 'primary_category') ) {
 
         if(! is_wp_error($primary_category) ) {
             return $primary_category;
@@ -232,15 +258,15 @@ function pc_get_primary_category( $post_id )
 
 
 /**
- * Helper function to return link to the primary category of a post. Return false if none is found.
+ * Helper function to return url to the primary category of a post. Return false if none is found.
  *
  * @param  $post_id {int|WP_Post} post id or a WP_Post object
  * @return mixed
  */
-function pc_get_primary_category_link( $post_id )
+function pc_primary_category_url( $post_id )
 {
 
-    if($primary_category = get_the_terms($post_id, "primary_category", true) ) {
+    if($primary_category = get_the_terms($post_id, 'primary_category' ) ) {
 
         if(! is_wp_error($primary_category) ) {
             if($category = get_category_by_slug($primary_category[0]->slug) ) {
@@ -252,3 +278,24 @@ function pc_get_primary_category_link( $post_id )
     return false;
 }
 
+
+
+/**
+ * Helper function to return html link to the primary category of a post. Return false if none is found.
+ *
+ * @param  $post_id {int|WP_Post} post id or a WP_Post object
+ * @param  $text {string} Link text
+ * @return mixed
+ */
+function pc_primary_category_link( $post_id, $text )
+{
+    $category = pc_primary_category_url($post_id);
+
+    if( false === $category ) {
+        return false;
+    }
+
+    $text = ( empty($text) ) ? $category->term_id : $text;
+	return '<a href="' . get_category_link($category->term_id) . '">' . $text . "</a>";
+
+}
